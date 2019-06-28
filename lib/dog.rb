1b0a1e6d4ee0ec507c56a3014c6fc7e909cfb721
Amanda E Nelson <amanda.nelson@yahoo.com>
2019-06-28 15:50:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,9 @@
 class Dog
- def bark
-   puts "Woof!"
- end
- def sit
-   puts "The Dog is sitting"
- end
+  def name=(dog_name)
+    this_dogs_name = dog_name
+  end
+
+  def name
+    this_dogs_name
+  end
 end
